f79da3c042a4ed3c889acc663479678a203780c6
KMBedw <105935281+KMBedw@users.noreply.github.com>
2024-07-07 17:02:31 +0200

    Delete Documents directory

--- Documents/CICD/Localstack/src/app.js
+++
@@ -1,7 +0,0 @@
-const user =  require('./data/user') 
-
-const calculate_age = require('./helpers/calculate-age')
-
-// console.log(user.name);
-// console.log(user.birth);
-console.log(calculate_age(user));
--- Documents/CICD/Localstack/src/data/user.js
+++
@@ -1,6 +0,0 @@
-const user = {
-  name: 'John DOE',
-  // Assurez-vous que la propriété 'birth' est une instance de Date
-  birth: new Date('1992-05-09'), // Cette ligne a été rétablie à une Date
-};
-module.exports = user;
